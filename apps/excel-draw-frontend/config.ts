07e05c1d2b189034de25560c0332b50c92ca7eeb
Your New Username <piyushchoudhary.068@gmail.com>
2025-05-14 09:46:29 +0530

    fix bugs

--- apps/excel-draw-frontend/config.ts
+++ apps/excel-draw-frontend/config.ts
@@ -1,2 +1,2 @@
-export  const  BACKEND_URL="http://localhost:4004"
-export const WS_URL="ws://localhost:8090"
+export  const  BACKEND_URL="https://draw-h4f0.onrender.com"
+export const WS_URL="https://ws-backend-t4ps.onrender.com"
