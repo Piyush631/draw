0566ff7169e6fed8a714d23abac1423f12a12e8f
Your New Username <piyushchoudhary.068@gmail.com>
2025-05-15 10:27:50 +0530

    fix ws

--- packages/common-backend/src/index.ts
+++ packages/common-backend/src/index.ts
@@ -2,5 +2,3 @@ export const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
 export const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/excel_draw';
 export const PORT = process.env.PORT || 3000;
 export const WS_PORT = process.env.WS_PORT || 8090;
-
-export * from './config';
