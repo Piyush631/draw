fa66f20d526991df72641880bc9fffb2eaba35a4
Your New Username <piyushchoudhary.068@gmail.com>
2025-05-17 08:58:04 +0530

    fix ws

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -34,7 +34,14 @@ function checkuser(token: string): string | null {
 
 ws.on("connection", function connection(ws, request) {
   const url = request.url;
+  console.log(url);
   if (!url) {
+    console.log("No URL provided");
+    console.log(request);
+    console.log(ws);
+    console.log(request.headers);
+    console.log(request.socket);
+
     ws.close(1008, "No URL provided");
     return;
   }
