559bfc8e8062b8b30f3c47903f851cf259096ac3
Your New Username <piyushchoudhary.068@gmail.com>
2025-05-17 10:15:46 +0530

    fix bcrypt

--- packages/common-backend/src/index.ts
+++ packages/common-backend/src/index.ts
@@ -1,4 +1,4 @@
 export const JWT_SECRET ="piyushchoudhary";
-export const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/excel_draw';
+export const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://database_owner:npg_gGc2jlWXk9Bi@ep-tiny-snowflake-a5c80a2z-pooler.us-east-2.aws.neon.tech/database?sslmode=require';
 export const PORT = process.env.PORT || 3000;
 export const WS_PORT = process.env.WS_PORT || 8090;
